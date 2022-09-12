64c9cd0882701fe6e2abe7d94233d69837591970
igot-thecode <89433099+igot-thecode@users.noreply.github.com>
2022-09-12 15:49:49 -0600

    1 file modified

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('Hello World!')
+console.log('Loading Server...')
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+//load middleware
+
+//REST routes
+//CRUDL
+//C post(server id)
+
+//R get 1
+
+//U put(client id, full replace), patch(partial replace)
+
+//D delete
+
+//L get all
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
+
+// let msg = new SpeechSynthesisUtterance('Hello World!')
+// window.speechSynthesis.speak(msg)
